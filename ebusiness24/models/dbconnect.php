09935a93c857310789ee6ee2a41b562cac613d07
nkmnz <42554376+nkmnz@users.noreply.github.com>
2018-11-11 20:12:29 +0100

    Add files via upload

---
+++ ebusiness24/models/dbconnect.php
@@ -0,0 +1,29 @@
+<?php
+function db_connect() {
+
+        // Define connection as a static variable, to avoid connecting more than once 
+    static $connection;
+
+        // Try and connect to the database, if a connection has not been established yet
+    if(!isset($connection)) {
+             // Load configuration as an array. Use the actual location of your configuration file
+        $config = array getenv(); 
+        $connection = mysqli_connect($config['DATABASE_HOST'],$config['DB_USERNAME'],$config['DB_PASSWORD'],$config['DB_DATABASE']);
+    }
+
+        // If connection was not successful, handle the error
+    if($connection === false) {
+            // Handle error - notify administrator, log to a file, show an error screen, etc.
+        return mysqli_connect_error(); 
+    }
+    return $connection;
+}
+
+// Connect to the database
+$connection = db_connect();
+
+// Check connection
+if ($connection->connect_error) {
+    die("Connect Error: " . $connection->connect_error);
+}
+?> 
