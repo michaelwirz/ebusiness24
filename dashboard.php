2c0071af006ac009eb7d0125fd579a3dc2940ef1
nk <nk@edukat.io>
2018-11-12 15:43:49 +0100

    add

--- dashboard.php
+++ dashboard.php
@@ -7,34 +7,6 @@
       </div>
     </div>
     <div class="main-panel">
-      <!-- Navbar -->
-      <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
-        <div class="container-fluid">
-          <div class="navbar-wrapper">
-            <a class="navbar-brand" href="#pablo">Dashboard</a>
-          </div>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="navbar-toggler-icon icon-bar"></span>
-            <span class="navbar-toggler-icon icon-bar"></span>
-            <span class="navbar-toggler-icon icon-bar"></span>
-          </button>
-          <div class="collapse navbar-collapse justify-content-end">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href="mailto:mail@nicolaskatte.de">
-                  <i class="material-icons">mail_outline</i> IT-Kontaktieren
-                </a>
-              </li>
-              <!-- your navbar here -->
-            </ul>
-          </div>
-        </div>
-      </nav>
-      <!-- End Navbar -->
-      <div class="content">
-        <div class="container-fluid">
-          <!-- your content here -->
-        </div>
-      </div>
+      <?php include 'navbar.php'; ?>
+    <?php include 'add.php'; ?>
     <div><?php include 'footer.php' ?></div>
