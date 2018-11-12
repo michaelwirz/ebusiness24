40ffd0b4a8147813552e861383b83db8083f6238
nk <nk@edukat.io>
2018-11-12 16:10:33 +0100

    remove2

--- add.php
+++ add.php
@@ -1,56 +1,38 @@
-<div class="content">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-md-6">
-              <div class="card">
-                <div class="card-header card-header-primary">
-                  <h4 class="card-title">Neuen Warentyp anlegen</h4>
-                  <p class="card-category">Geben Sie den Produktnamen, den Einkaufs- sowie den Verkaufspreis an. ID wird automatisch vergeben.</p>
-                </div>
-                <div class="card-body">
-                  <form>
-                    <div class="row">
-                      <div class="col-md-8">
-                        <div class="form-group">
-                          <label class="bmd-label-floating">Produktname</label>
-                          <input type="text"
-                                 required="1"
-                                 class="form-control">
-                        </div>
-                      </div>
-                    </div>
-                    <div class="row">
-                      <div class="col-md-4">
-                        <div class="form-group">
-                          <label class="bmd-label-floating">Einkaufspreis</label>
-                          <input min="0"
-                                 step="0.01"
-                                 value="0"
-                                 placeholder=" "
-                                 required="1"
-                                 type="number" class="form-control">
-                        </div>
-                      </div>
-                    </div>
-                    <div class="row">
-                      <div class="col-md-4">
-                        <div class="form-group">
-                          <label class="bmd-label-floating">Verkaufspreis</label>
-                          <input min="0"
-                                 step="0.01"
-                                 value="0"
-                                 placeholder=" "
-                                 required="1"
-                                 type="number" class="form-control">
-                        </div>
-                      </div>
-                    </div>
-                    <button type="submit" class="btn btn-primary pull-right">Eintragen</button>
-                    <div class="clearfix"></div>
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
+<div class="col-md-6">
+    <div class="card">
+        <div class="card-header card-header-primary">
+            <h4 class="card-title">Neuen Warentyp anlegen</h4>
+            <p class="card-category">Geben Sie den Produktnamen, den Einkaufs- sowie den Verkaufspreis an. ID wird automatisch vergeben.</p>
         </div>
-      </div>
+        <div class="card-body">
+            <form>
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="form-group">
+                            <label class="bmd-label-floating">Produktname</label>
+                            <input type="text" required="1" class="form-control">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label class="bmd-label-floating">Einkaufspreis</label>
+                            <input min="0" step="0.01" value="0" placeholder=" " required="1" type="number" class="form-control">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label class="bmd-label-floating">Verkaufspreis</label>
+                            <input min="0" step="0.01" value="0" placeholder=" " required="1" type="number" class="form-control">
+                        </div>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary pull-right">Eintragen</button>
+                <div class="clearfix"></div>
+            </form>
+        </div>
+    </div>
+</div>
--- dashboard.php
+++ dashboard.php
@@ -8,6 +8,12 @@
     </div>
     <div class="main-panel">
       <?php include 'navbar.php'; ?>
+        <div class="content">
+        <div class="container-fluid">
+          <div class="row">
     <?php include 'add.php'; ?>
     <?php include 'remove.php'; ?>
+                        </div>
+        </div>
+      </div>
     <div><?php include 'footer.php' ?></div>
