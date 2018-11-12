ff20f2e9d3abfad11a8b8a8f0372b958a3baf7ed
nk <nk@edukat.io>
2018-11-12 08:16:15 +0100

    Update _views/navigation.php

--- _views/navigation.php
+++ _views/navigation.php
@@ -1,12 +1,8 @@
-<?php
-
-    <ul>
-        <li><a href="">Warenbestand</a></li>
-        <li><a href="">Zugang erfassen</a></li>
-        <li><a href="">Abgang erfassen</a></li>
-        <li><a href="">Übersicht Lieferungen</a></li>
-        <li><a href="">Übersicht Bestellungen</a></li>
-        <li><a href="">IT kontaktieren</a></li>
-    </ul>
-
-?>
+<ul>
+    <li><a href="">Warenbestand</a></li>
+    <li><a href="">Zugang erfassen</a></li>
+    <li><a href="">Abgang erfassen</a></li>
+    <li><a href="">Übersicht Lieferungen</a></li>
+    <li><a href="">Übersicht Bestellungen</a></li>
+    <li><a href="">IT kontaktieren</a></li>
+</ul>
