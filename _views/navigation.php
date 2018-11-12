4f9aac575b8a69e601f4a550d2ea16b37d56e278
nk <nk@edukat.io>
2018-11-12 14:22:44 +0100

    überflüssiges gelöscht

--- _views/navigation.php
+++
@@ -1,8 +0,0 @@
-<ul>
-    <li><a href="">Warenbestand</a></li>
-    <li><a href="">Zugang erfassen</a></li>
-    <li><a href="">Abgang erfassen</a></li>
-    <li><a href="">Übersicht Lieferungen</a></li>
-    <li><a href="">Übersicht Bestellungen</a></li>
-    <li><a href="">IT kontaktieren</a></li>
-</ul>
