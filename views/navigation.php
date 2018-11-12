ae9fbf3089d5f699501b1bf2e3f04b8b547f7fb5
nk <nk@edukat.io>
2018-11-12 08:01:07 +0100

    navigation

---
+++ views/navigation.php
@@ -0,0 +1,13 @@
+<?php
+
+<div id="menu_left">
+    <ul>
+        <li><a href="">Warenbestand</a></li>
+        <li><a href="">Zugang erfassen</a></li>
+        <li><a href="">Abgang erfassen</a></li>
+        <li><a href="">Übersicht Lieferungen</a></li>
+        <li><a href="">Übersicht Bestellungen</a></li>
+        <li><a href="">IT kontaktieren</a></li>
+    </ul>
+</div>
+?>
