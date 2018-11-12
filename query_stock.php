88314471404d724271f29aee05bfa4daae780aaf
nk <nk@edukat.io>
2018-11-12 14:04:28 +0100

    Update query_stock.php

--- query_stock.php
+++ query_stock.php
@@ -1,7 +1,7 @@
 <?php
 require_once('db_connect.php');
 $sql = "SELECT Waren_ID, Name, Einkaufspreis, Verkaufspreis FROM ware";
-$result = $connection->query($sql);
+$result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
