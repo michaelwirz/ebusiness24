38038b217e22ca10e6a417e16af4873cb6613f8b
nk <nk@edukat.io>
2018-11-13 19:30:08 +0100

    Update add.php

--- add.php
+++ add.php
@@ -8,7 +8,7 @@ if (isset($_POST['submit'])) {
     echo $einkaufspreis;
     echo $verkaufspreis;
     $sql= "INSERT INTO ware (Waren_ID, Name, Einkaufspreis, Verkaufspreis) 
-    VALUES ('5', $name', '$einkaufspreis', '$verkaufspreis')";
+    VALUES ('5', '$name', '$einkaufspreis', '$verkaufspreis')";
     
     if (mysqli_query($connection, $sql)) {
         echo "New record created successfully";
