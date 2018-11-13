8f65ff1171f34dedb13358d3a57e78c4537b29c5
nk <nk@edukat.io>
2018-11-13 09:54:56 +0100

    add geändert

--- add.php
+++ add.php
@@ -7,3 +7,50 @@
     $sql= "INSERT INTO ware (Name, Einkaufspreis, Verkaufspreis) VALUES ('$name', '$einkaufspreis', '$verkaufspreis')";
     }
 ?>
+
+<div class="col-md-6">
+    <div class="card">
+        <div class="card-header card-header-success">
+            <h4 class="card-title">Warentyp hinzufügen</h4>
+            <p class="card-category">Produktname, Einkaufs- und Verkaufspreis sind erforderlich</p>
+        </div>
+        <div class="card-body">
+            <form role="form" method="post" action="dashboard.php">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group" >
+                            <label class="bmd-label-floating">Produkt-ID</label>
+                            <input type="text" class="form-control" disabled>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-8">
+                        <div class="form-group">
+                            <label class="bmd-label-floating">Produktname</label>
+                            <input type="text" required="1" class="form-control" name="Name">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label class="bmd-label-floating">Einkaufspreis</label>
+                            <input min="0" step="0.01" value="0" placeholder="wird automatisch vergeben" required="1" type="number" class="form-control"  name="Einkaufspreis">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label class="bmd-label-floating">Verkaufspreis</label>
+                            <input min="0" step="0.01" value="0" placeholder=" " required="1" type="number" class="form-control"  name="Verkaufspreis">
+                        </div>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-info pull-right">Eintragen</button>
+                <div class="clearfix"></div>
+            </form>
+        </div>
+    </div>
+</div>
