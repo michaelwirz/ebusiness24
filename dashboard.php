12db61a1ae104004c6f34544a43b635a81ba2980
nk <nk@edukat.io>
2018-11-13 15:03:25 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,13 @@
 <?php 
 include 'head.php';
 $page = "Dashboard";
+    if (isset($_POST['submit'])) {
+    require_once('db_connect.php');
+    $name = $_POST['Name']; 
+    $einkaufspreis = $_POST['Einkaufspreis'];
+    $verkaufspreis = $_POST['Verkaufspreis'];
+    $sql= "INSERT INTO ware (Name, Einkaufspreis, Verkaufspreis) VALUES ('$name', '$einkaufspreis', '$verkaufspreis')";
+    }
 ?>
 
 <body class="">
