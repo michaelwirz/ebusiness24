6e618b17a735273541adbed97f7f1f3f8c12edb2
nk <nk@edukat.io>
2018-11-14 09:54:04 +0100

    Update removed.php

--- removed.php
+++ removed.php
@@ -3,11 +3,10 @@ if (isset($_POST['remove'])) {
     require_once('db_connect.php');
     $remove_waren_id = intval($_POST['remove']);
     echo $remove_waren_id;
-    echo $connection;
    // $sql = "SELECT * INTO ware_removed
    // FROM ware
    // WHERE Waren_ID = '$remove_waren_id'";
-    $sql = "DELETE FROM ware WHERE Waren_ID = '$remove_waren_id'";
+    $sql = "DELETE FROM ware WHERE Waren_ID = 5";
     mysqli_close($connection);
 }
 ?>
