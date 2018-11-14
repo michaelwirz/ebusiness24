fceeb3a87306f5983f3c07fc799a7f8b3ccca3db
nk <nk@edukat.io>
2018-11-14 22:19:53 +0100

    public

---
+++ public/includes/process_login.php
@@ -0,0 +1,22 @@
+<?php
+// Quelle: https://de.wikihow.com/Ein-sicheres-Login-Skript-mit-PHP-und-MySQL-erstellen
+include_once 'db_connect.php';
+include_once 'functions.php';
+ 
+sec_session_start(); // Unsere selbstgemachte sichere Funktion um eine PHP-Sitzung zu starten.
+ 
+if (isset($_POST['email'], $_POST['p'])) {
+    $email = $_POST['email'];
+    $password = $_POST['p']; // Das gehashte Passwort.
+ 
+    if (login($email, $password, $mysqli) == true) {
+        // Login erfolgreich 
+        header('Location: ../dashboard.php');
+    } else {
+        // Login fehlgeschlagen 
+        header('Location: ../index.php?error=1');
+    }
+} else {
+    // Die korrekten POST-Variablen wurden nicht zu dieser Seite geschickt. 
+    echo 'Invalid Request';
+}
