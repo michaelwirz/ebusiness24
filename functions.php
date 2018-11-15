c69fe0a40668a636aca36fb96130b84b42d8e188
nk <nk@edukat.io>
2018-11-15 13:56:37 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,15 +2,16 @@
 
 //füge neuen Warentypen zum Lager hinzu
 function AddNewProduct() {
+    global $connection;
     $name = htmlspecialchars($_POST['Name']); 
     $einkaufspreis = (float) $_POST['Einkaufspreis'];
     $verkaufspreis = (float) $_POST['Verkaufspreis'];
     $sql= "INSERT INTO ware (Name, Einkaufspreis, Verkaufspreis) 
     VALUES ('$name', '$einkaufspreis', '$verkaufspreis')";
     if (mysqli_query($connection, $sql)) {
-//        echo "New record created successfully";
+       echo "New record created successfully";
     } else {
-//        echo "Error: " . $sql . "<br>" . mysqli_error($connection);
+        echo "Error: " . $sql . "<br>" . mysqli_error($connection);
     mysqli_close($connection);
     }
 }
